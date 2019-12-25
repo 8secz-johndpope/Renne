7bd44162dd58125bf6d15c5ff28f7d9c9b7c981d
Joinn99 <joinnnor@outlook.com>
2019-12-25 17:08:29 +0800

    version 2.0

--- setup.sh
+++ setup.sh
@@ -4,8 +4,11 @@ apt install -y libsm6 libxrender1 libfontconfig1 libxext6 libxrender-dev
 apt install -y python3-pip
 git clone https://github.com/Joinn99/Renne
 pip3 install -r requirements.txt 
-cd Renne
 cd models
 wget https://www.dropbox.com/s/nah0kwsralwda0b/pose2seg-full.pth
 wget https://www.dropbox.com/s/9ubmrc0chgedgza/openpose-pytorch.pth
+wget https://docs.google.com/uc?id=1G8lXquU3eREfs8KorFpFC8N4YmTQRksF&export=download
+mv 1G8lXquU3eREfs8KorFpFC8N4YmTQRksF EdgeModel_gen.pth
+wget https://docs.google.com/uc?id=1_oYnmK7kppXqka9UUsHrZB4gWE4ouSgT&export=download
+mv 1_oYnmK7kppXqka9UUsHrZB4gWE4ouSgT InpaintingModel_gen.pth
 cd ..
